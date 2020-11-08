Make predict to work after fixing normalization bug

code/train.py
@@ -20,7 +20,7 @@ decay_rate = 0
 decay_epochs = 50
 momentum = 0.9
 batch_sizes = [16]
-epochs = 300
+epochs = 10
 plot = True
 train = False
 predict = True
@@ -31,7 +31,7 @@ params['resetHistory']  = False
 params['print_summary'] = True
 params['dropout'] = 0.5
 params['data_aug_enable'] = False
-params['models_dir'] = None
+params['models_dir'] = '../trained_models/vanilla'
 
 # data files
 data_in_dir = "../data"
@@ -87,16 +87,18 @@ def lr_scheduler(epoch, lr):
     return lr
 
 ## Define the model
-x_train, y_train, c_train = load_data_from_file(train_data_file, "train")
+if train:
+    x_train, y_train, c_train = load_data_from_file(train_data_file, "train")
 x_dev, y_dev, c_dev = load_data_from_file(dev_data_file, "dev")
 
 # reshape
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+if train:
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
 x_dev = np.reshape(x_dev, (x_dev.shape[0], x_dev.shape[1], x_dev.shape[2], 1))
 
 for batch_size in batch_sizes:
     for lr in learning_rates:
-        if not params['models_dir']:
+        if train or 'models_dir' not in params:
             params['models_dir'] = f'../experiments/{model_name}/{batch_size}.{lr}'
         history = LossHistory()
         if model_name == 'vanilla':
@@ -117,11 +119,13 @@ for batch_size in batch_sizes:
         model.compile(optimizer)
 
         #Load data into model
-        model.x_train = x_train
-        model.y_train = y_train
+        if train:
+            model.x_train = x_train
+            model.y_train = y_train
+            print(model.x_train.dtype)
+
         model.x_val = x_dev
         model.y_val = y_dev
-        print (model.x_train.dtype)
         print (model.x_val.dtype)
 
         # instantiate model
@@ -134,9 +138,10 @@ for batch_size in batch_sizes:
             model.train_plot(fig, ax, show_plot=False)
 
         if predict:
-            y_hat = model.predict(x_dev, batch_size)
+            y_hat = model.my_predict(x_dev, batch_size)
+            y_pred = y_hat > model.params['y_hat_threshold']
             n = y_dev.shape[0]
-            y_err = y_hat != y_dev.reshape(n,1)
+            y_err = y_pred != y_dev.reshape(n,1)
             acc = (n - np.sum(y_err))*100/n
             print ("Accuracy on dev set is %.2f" %(acc))
 

models/base_model.py
@@ -98,15 +98,15 @@ class BaseModel:
         self.model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
     def train(self, batch_size, epochs, lr_scheduler):
+        if self.x_train.dtype == np.uint8:
+            norm_const = np.array(255).astype('float16')
+            self.x_train = self.x_train / norm_const
+
+        if self.x_val.dtype == np.uint8:
+            norm_const = np.array(255).astype('float16')
+            self.x_val = self.x_val / norm_const
+
         if self.params['data_aug_enable']:
-            if self.x_train.dtype == np.uint8:
-                norm_const = np.array(255).astype('float16')
-                self.x_train = self.x_train / norm_const
-
-            if self.x_val.dtype == np.uint8:
-                norm_const = np.array(255).astype('float16')
-                self.x_val = self.x_val / norm_const
-
             self.datagen = ImageDataGenerator(rotation_range = 40, width_shift_range=0.3, height_shift_range=0.3)
             self.model.fit_generator(self.datagen.flow(self.x_train, self.y_train, batch_size=batch_size),
                                      steps_per_epoch=len(self.x_train)/batch_size,
@@ -117,14 +117,6 @@ class BaseModel:
             return
 
         # default
-        if self.x_train.dtype == np.uint8:
-            norm_const = np.array(255).astype('float16')
-            self.x_train = self.x_train / norm_const
-
-        if self.x_val.dtype == np.uint8:
-            norm_const = np.array(255).astype('float16')
-            self.x_val = self.x_val / norm_const
-
         self.model.fit(self.x_train, self.y_train, batch_size=batch_size, epochs=epochs,
                            validation_data=(self.x_val, self.y_val),
                            callbacks = [self.history,
@@ -158,5 +150,8 @@ class BaseModel:
             plt.show()
 
     # over write predict method
-    def predict(self, x, batchsize):
+    def my_predict(self, x, batchsize):
+        if x.dtype == np.uint8:
+            norm_const = np.array(255).astype('float16')
+            x = x / norm_const
         return self.model.predict(x, batchsize)